4d0fb9a830d9baae97cc46aee7e35fab6700ceff
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-11-15 13:31:50 +0100

    api fix blogs

--- js/api.js
+++ js/api.js
@@ -1,23 +1,21 @@
-const url =
-  "https://willand.tech/blog/wp-json/wp/v2/posts/?consumer_key=ck_d3b6f472d19e7c6bd1a1fc3044c8d9681a2cd8e1?consumer_secret=cs_27e6a8da637b31c79a1092c7a6949163c02acbad";
+const url = `https://willand.tech/blog/wp-json/wp/v2/posts?per_page=10`;
+const showAllUrl = `https://willand.tech/blog/wp-json/wp/v2/posts?per_page=100`;
 const blogList = document.querySelector(".blog-list");
 const contentContainer = document.querySelector(".content-container");
+const showAll = document.querySelector(".show-all");
+
 async function getBlogs() {
   try {
     const response = await fetch(url);
     const results = await response.json();
-    console.log(results);
 
     for (let i = 0; i < results.length; i++) {
-      if (i === 10) {
-        break;
-      }
       blogList.innerHTML += `
       <div class="blog-card">
       <a href="post.html?id=${results[i].id}">
         <img src="${results[i].jetpack_featured_media_url}">
-            <h2>${results[i].title.rendered}</h2>
-                <h3>${results[i].excerpt.rendered}</h3>
+        <h2>${results[i].title.rendered}</h2>
+        <h3>${results[i].excerpt.rendered}</h3>
         </a>
     </div>`;
     }
@@ -29,10 +27,28 @@ async function getBlogs() {
 
 getBlogs(url);
 
-//   for (let i = 0; i < blogList.length; i++) {
-//     results.forEach(function (results) {
-//       blogList[
-//         i
-//       ].innerHTML += `<div class="blog-card"><h2>${results.id}</h2><img src"${results.jetpack_featured_media_url[0]}"></div>`;
-//     });
-//   }
+showAll.addEventListener("click", () => {
+  blogList.innerHTML = "";
+  getAllBlogs(showAllUrl);
+});
+
+async function getAllBlogs() {
+  try {
+    const response = await fetch(showAllUrl);
+    const results = await response.json();
+
+    for (let i = 0; i < results.length; i++) {
+      blogList.innerHTML += `
+      <div class="blog-card">
+      <a href="post.html?id=${results[i].id}">
+        <img src="${results[i].jetpack_featured_media_url}">
+        <h2>${results[i].title.rendered}</h2>
+        <h3>${results[i].excerpt.rendered}</h3>
+        </a>
+    </div>`;
+    }
+  } catch (error) {
+    contentContainer.innerHTML = "<p>An error has occured</p>";
+    console.log(error);
+  }
+}
--- js/blog-post.js
+++ js/blog-post.js
@@ -4,7 +4,7 @@ const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-const url = `https://willand.tech/blog/wp-json/wp/v2/posts/${id}?consumer_key=ck_d3b6f472d19e7c6bd1a1fc3044c8d9681a2cd8e1?consumer_secret=cs_27e6a8da637b31c79a1092c7a6949163c02acbad`;
+const url = `https://willand.tech/blog/wp-json/wp/v2/posts/${id}`;
 
 const postImage = document.querySelector(".carousel");
 const postHeader = document.querySelector(".carousel-info");
