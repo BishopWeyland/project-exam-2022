630bb8588c21cc7c713c363b9d17045c22e6260f
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-11-16 11:39:32 +0100

    show all simpler

--- js/api.js
+++ js/api.js
@@ -1,10 +1,10 @@
-const url = `https://willand.tech/blog/wp-json/wp/v2/posts?per_page=10`;
+const url = `https://willand.tech/blog/wp-json/wp/v2/posts`;
 const showAllUrl = `https://willand.tech/blog/wp-json/wp/v2/posts?per_page=100`;
 const blogList = document.querySelector(".blog-list");
 const contentContainer = document.querySelector(".content-container");
-const showAll = document.querySelector(".show-all");
+const showAllButton = document.querySelector(".show-all");
 
-async function getBlogs() {
+async function getBlogs(url) {
   try {
     const response = await fetch(url);
     const results = await response.json();
@@ -30,28 +30,7 @@ async function getBlogs() {
 
 getBlogs(url);
 
-showAll.addEventListener("click", () => {
+showAllButton.addEventListener("click", () => {
   blogList.innerHTML = "";
-  getAllBlogs(showAllUrl);
+  getBlogs(showAllUrl);
 });
-
-async function getAllBlogs() {
-  try {
-    const response = await fetch(showAllUrl);
-    const results = await response.json();
-
-    for (let i = 0; i < results.length; i++) {
-      blogList.innerHTML += `
-      <div class="blog-card">
-      <a href="post.html?id=${results[i].id}">
-        <img src="${results[i].jetpack_featured_media_url}">
-        <h2>${results[i].title.rendered}</h2>
-        <h3>${results[i].excerpt.rendered}</h3>
-        </a>
-    </div>`;
-    }
-  } catch (error) {
-    contentContainer.innerHTML = "<p>An error has occured</p>";
-    console.log(error);
-  }
-}
