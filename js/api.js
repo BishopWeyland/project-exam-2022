efc21d56e38725235c1cd75d3b7f58975b9fbaa6
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-12-01 14:32:59 +0100

    cleaning code on api.js

--- js/api.js
+++ js/api.js
@@ -1,9 +1,6 @@
 const url = `https://willand.tech/blog/wp-json/wp/v2/posts`;
 const blogList = document.querySelector(".blog-list");
 const contentContainer = document.querySelector(".content-container");
-const categories = document.querySelectorAll(".category");
-const searchButton = document.querySelector(".search-button");
-const showMore = document.querySelector(".show-more");
 
 // Get array of blog-posts from API.
 
@@ -17,21 +14,25 @@ async function getBlogs(url) {
       blogList.innerHTML += `
       <div class="blog-card">
         <a href="post.html?id=${results[i].id}">
-            <img src="${results[i].jetpack_featured_media_url}">
-            
-            <h2>${results[i].title.rendered}</h2>
-            ${results[i].excerpt.rendered}
+        <img src="${results[i].jetpack_featured_media_url}">
+        <h2>${results[i].title.rendered}</h2>
+        ${results[i].excerpt.rendered}
         </a>
     </div>`;
     }
   } catch (error) {
-    contentContainer.innerHTML = `<div class="error-message"><p><i class="fa-solid fa-circle-exclamation"></i>I am sorry, an error has occured. Please try to refresh the page.</p>
-    <p>${error}</p></div>`;
+    contentContainer.innerHTML = `
+      <div class="error-message">
+        <p><i class="fa-solid fa-circle-exclamation"></i>I am sorry, an error has occured. Please try to refresh the page.</p>
+        <p>${error}</p>
+      </div>`;
   }
 }
 getBlogs(url);
 
 // Show 10 more results each time the "show-more" button is pressed.
+
+const showMore = document.querySelector(".show-more");
 
 let morePages = 20;
 let moreUrl = `${url}?per_page=${morePages}`;
@@ -51,20 +52,25 @@ async function getMoreBlogs() {
     for (let i = 0; i < results.length; i++) {
       blogList.innerHTML += `
       <div class="blog-card">
-      <a href="post.html?id=${results[i].id}">
-        <img src="${results[i].jetpack_featured_media_url}">
-        <h2>${results[i].title.rendered}</h2>
-        <h3>${results[i].excerpt.rendered}</h3>
+        <a href="post.html?id=${results[i].id}">
+          <img src="${results[i].jetpack_featured_media_url}">
+          <h2>${results[i].title.rendered}</h2>
+          <h3>${results[i].excerpt.rendered}</h3>
         </a>
-    </div>`;
+      </div>`;
     }
   } catch (error) {
-    contentContainer.innerHTML = `<div class="error-message"><p><i class="fa-solid fa-circle-exclamation"></i>I am sorry, an error has occured. Please try to refresh the page.</p>
-    <p>${error}</p></div>`;
+    contentContainer.innerHTML = `
+    <div class="error-message">
+      <p><i class="fa-solid fa-circle-exclamation"></i>I am sorry, an error has occured. Please try to refresh the page.</p>
+      <p>${error}</p>
+    </div>`;
   }
 }
 
 // Use radio buttons to show posts by category.
+
+const categories = document.querySelectorAll(".category");
 
 categories.forEach(function (category) {
   category.onclick = function (event) {
@@ -81,6 +87,10 @@ categories.forEach(function (category) {
     getBlogs(newUrl);
   };
 });
+
+// search for posts.
+
+const searchButton = document.querySelector(".search-button");
 
 searchButton.onclick = function () {
   searchInput = document.querySelector(".search-input").value;
