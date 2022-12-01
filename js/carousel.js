9442a46b51841098cd6eb786743124379976eee3
Håkon Willand Engebretsen <willand27@hotmail.com>
2022-12-01 14:51:29 +0100

    clean carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -11,11 +11,14 @@ async function getCarousel(url) {
     let a = 0;
 
     function carouselFunctionality() {
-      track.innerHTML = `<a class="slide" href="post.html?id=${results[a].id}"><img src="${results[a].jetpack_featured_media_url}">`;
+      track.innerHTML = `
+      <a class="slide" href="post.html?id=${results[a].id}">
+      <img src="${results[a].jetpack_featured_media_url}">`;
 
-      carouselInfo.innerHTML = `<h2>${results[a].title.rendered}</h2>
-        ${results[a].excerpt.rendered}
-        <a href="post.html?id=${results[a].id}" class="btn">Read more</a>`;
+      carouselInfo.innerHTML = `
+      <h2>${results[a].title.rendered}</h2>
+      ${results[a].excerpt.rendered}
+      <a href="post.html?id=${results[a].id}" class="btn">Read more</a>`;
     }
 
     carouselFunctionality();
@@ -36,8 +39,11 @@ async function getCarousel(url) {
       carouselFunctionality();
     });
   } catch (error) {
-    carouselInfo.innerHTML = `<div class="error-message"><p><i class="fa-solid fa-circle-exclamation"></i>I am sorry, an error has occured. Please try to refresh the page.</p>
-    <p>${error}</p></div>`;
+    carouselInfo.innerHTML = `
+    <div class="error-message">
+      <p><i class="fa-solid fa-circle-exclamation"></i>I am sorry, an error has occured. Please try to refresh the page.</p>
+      <p>${error}</p>
+    </div>`;
   }
 }
 
